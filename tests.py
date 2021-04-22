097c7e4609421e92c8dbe017e677b97460563475
Winston <winston@nitidbit.com>
2021-04-21 22:06:30 -0700

    trim_margin()

--- tests.py
+++ tests.py
@@ -3,7 +3,9 @@ to run tests:
     python3 -m pytest -v main.py
 """
 
+import re
 import pytest
+
 from parser import parse
 
 class Color:
@@ -12,6 +14,21 @@ class Color:
     reset = '\x1b[39;49m'
     #  grey = '\x1b[90m'
     default = '\x1b[39m'
+
+def trim_margin(str):
+    '''Trim left margin of spaces. The last line is used to gauge how where the margin is.'''
+    lines = str.split('\n')
+    last_line = lines[-1]
+    margin_width = len(last_line)
+    lines = map(lambda l: l[margin_width:], lines)
+    return '\n'.join(lines)
+
+def test_trim_margin():
+    str = '''
+        abc
+        def
+        '''
+    assert trim_margin(str) == '\nabc\ndef\n'
 
 def without_parseinfo(node):
     if isinstance(node, dict) and 'parseinfo' in node:
