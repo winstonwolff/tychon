6c837338dc191bc269de4c8662df54174edc333d
Winston <winston@nitidbit.com>
2021-12-30 21:19:21 -0800

    Add repl

--- tychon.py
+++ tychon.py
@@ -6,7 +6,10 @@ import _builtins
 from _builtins import Scope, Evaluator
 from pprint import pformat
 
-def run(code_str, stdout):
+def run_file(source_filename):
+
+    with open(source_fname, 'rt') as f:
+        code_str = f.read()
 
     for code_line in code_str.split('\n'):
         #  print('!!! code_line=', code_line)
@@ -17,12 +20,33 @@ def run(code_str, stdout):
         Evaluator.run(scope, ast)
     #  print('DONE. scope=', scope)
 
+CTRL_D = '\0x4'
+def repl(stdin, stdout):
+
+    print('Tython REPL')
+
+    try:
+        while True:
+            code_line = input('>>> ')
+
+            ast = _parser.parse(code_line)
+            scope = Scope(_builtins.BUILTIN_FUNCTIONS)
+            scope.update({'stdout': stdout})
+            result = Evaluator.run(scope, ast)
+            print(repr(result))
+    except EOFError:
+        print()
+        return
+
+
 def main():
     #  print('!!! sys.argv=', sys.argv)
-    source_fname = sys.argv[1]
-    with open(source_fname, 'rt') as f:
-        code_str = f.read()
-        run(code_str, sys.stdout)
+    if len(sys.argv) > 1:
+        source_fname = sys.argv[1]
+        run_file(source_fname)
+
+    else:
+        repl(sys.stdin, sys.stdout)
 
 
 if __name__ == '__main__':
