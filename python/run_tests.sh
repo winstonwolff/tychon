fcd40df52b727c0c3d95b848d73b26fadd4d5e18
Winston <winston@nitidbit.com>
2022-04-20 20:05:45 -0700

    Switch to my fork of Tatsu

--- python/run_tests.sh
+++ python/run_tests.sh
@@ -4,8 +4,9 @@ set -x
 # pytest options:
 #       -k _ — only run tests with pattern of _, e.g. -k 'multiply or operator'
 
-python3 -m pytest -vv --capture=no --log-format="%(message)s"\
+python3 -m pytest -vv --log-format="%(message)s"\
   _parser.py \
   _parser_test.py \
   tychon_test.py $1 $2 $3 $4
 
+  # --capture=no \
