d8971e10558f0b973c612c13a03501d7bf844399
Winston <winston@nitidbit.com>
2023-09-29 16:07:08 -0700

    print() joins with spaces

--- wasm-as/assembly/interpeter.ts
+++ wasm-as/assembly/interpeter.ts
@@ -1,15 +1,18 @@
 import { JSON } from "assemblyscript-json/assembly"
 
-function call(args: JSON.Arr):JSON.Value {
-  const a = args.valueOf()
-  const functionName = a[0].toString()
-  console.log(`!!! call() functionName = ${functionName}`)
-  const f = lookup(functionName)
-  return f(a.slice(1))
+
+type List = Array<JSON.Value>
+type ArgumentList = Array<JSON.Value>
+type TyFunction = (args:ArgumentList) => JSON.Value
+
+export function evaluate(code: string): string {
+  let v: JSON.Arr = <JSON.Arr>(JSON.parse(code));
+  // console.log(`!!! evaluate() code = ${v.stringify()}`)
+  return call(v).toString()
 }
 
 function evalValue(value: JSON.Value): JSON.Value {
-  console.log(`!!! evalValue() value=${value.toString()}`)
+  // console.log(`!!! evalValue() value=${value.toString()}`)
   if (value.isArr) {
     return call(<JSON.Arr>value)
   } else {
@@ -17,25 +20,33 @@ function evalValue(value: JSON.Value): JSON.Value {
   }
 }
 
-export function evaluate(code: string): string {
-  let v: JSON.Arr = <JSON.Arr>(JSON.parse(code));
-  console.log(`!!! evaluate() code = ${v.stringify()}`)
-  return call(v).toString()
+function call(args: JSON.Arr):JSON.Value {
+  const a = args.valueOf()
+  const functionName = a[0].toString()
+  // console.log(`!!! call() functionName = ${functionName}`)
+  const f = lookup(functionName)
+  return f(a.slice(1))
 }
 
-type List = Array<JSON.Value>
-type AnyFunc = (l:List) => JSON.Value
+function lookup(functionName: string): TyFunction {
+  if (functionName === "print") return print
+  if (functionName === "list") return list
+  if (functionName === "module") return module
 
+  return print
+}
 
-function print(arguments:List):JSON.Value {
-  const msg = arguments.toString()
-  console.log("!!! print msg="); console.log(msg)
+function print(arguments: ArgumentList):JSON.Value {
+  // const msg = arguments.toString()
+  const msg:string = arguments.map<string>( function(v){ return v.toString() } ).join(" ")
+
+  console.log(`!!! print msg= "${msg}"`)
   return JSON.from(msg)
 }
 
-function list(arguments:List):JSON.Value {
+function list( arguments:ArgumentList ):JSON.Value {
   const msg = arguments.toString()
-  console.log('!!! list() msg='); console.log(msg)
+  // console.log('!!! list() msg='); console.log(msg)
   const result = new JSON.Arr()
   for(let i = 0; i < arguments.length; i++) {
     result.push(arguments[i])
@@ -44,22 +55,10 @@ function list(arguments:List):JSON.Value {
   return result
 }
 
-function mod(arguments:List): JSON.Value {
+function module(arguments:ArgumentList): JSON.Value {
   const result = new JSON.Arr()
   for(let i = 0; i < arguments.length; i++) {
     result.push(evalValue(arguments[i]))
   }
   return result
 }
-
-type TyValue = JSON.Value
-type TyFunction = (args:List) => TyValue
-
-function lookup(functionName: string): TyFunction {
-  if (functionName === "print") return print
-  if (functionName === "list") return list
-  if (functionName === "mod") return mod
-
-  return print
-}
-
