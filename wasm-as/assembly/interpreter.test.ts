cd6f7d4c1219eb364076f2a7c3ca907df0e29b7e
Winston <winston@nitidbit.com>
2023-10-06 17:56:04 -0700

    evaluates ["module" [...]]

--- wasm-as/assembly/interpreter.test.ts
+++ wasm-as/assembly/interpreter.test.ts
@@ -2,9 +2,18 @@ import { evaluate } from './interpreter'
 
 describe('interpreter.ts', ():void => {
   describe('evaluate()', ():void => {
-    test('can call print()', ():void => {
+
+    test('can [print "hello" "world"]', ():void => {
       const code = `["print", "hello", "world"]`
       expect(evaluate(code)).toStrictEqual('TyString(\"[hello world]\")')
+    })
+
+    test('evaluates [module]', ():void => {
+      const code = `["module",
+          ["print", "hello", "world"],
+          ["print", "I'm", "a", "Module"],
+        ]`
+      expect(evaluate(code)).toStrictEqual(`TyList(TyString("[hello world]") TyString("[I'm a Module]"))`)
     })
   })
 })
